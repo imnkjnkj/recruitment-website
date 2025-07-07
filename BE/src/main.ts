1dcff427baf9fab49db84e5a121e201a78182237
imnkjnkj <yuusakuragi123@gmail.com>
2025-07-07 22:48:35 +0700

    setup mogodb

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -2,13 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
   app.useStaticAssets(join(__dirname, '..', 'public')); //js, css, img
   app.setBaseViewsDir(join(__dirname, '..', 'views')); //view
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  const port = configService.get('PORT');
+
+  await app.listen(port);
 }
 bootstrap();
